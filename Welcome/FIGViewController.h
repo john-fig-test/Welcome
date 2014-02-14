c5e830e39a91c8b1238393738809b89411881524
John Figueiredo <john@johnfigueiredo.com>
2014-02-14 14:25:40 -0800

    Initial Commit

---
+++ Welcome/FIGViewController.h
@@ -0,0 +1,13 @@
+//
+//  FIGViewController.h
+//  Welcome
+//
+//  Created by John Figueiredo on 2/14/14.
+//  Copyright (c) 2014 John Figueiredo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FIGViewController : UIViewController
+
+@end
---
+++ Welcome/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Welcome
+//
+//  Created by John Figueiredo on 2/14/14.
+//  Copyright (c) 2014 John Figueiredo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FIGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([FIGAppDelegate class]));
+  }
+}
